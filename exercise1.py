02833e8572cc686d98d81eed7be821d2255d2192
efrainDL <36612356+efrainDL@users.noreply.github.com>
2018-02-18 19:59:52 -0800

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+money=input("How much money do you have? ")
+print("price of Nintendo Wii= Php 10,000")
+Nintendo=10000
+Wii=money/Nintendo 
+print"You can afford", Wii, "Nintendo Wii(s)"
+print"You need another", ((Wii*Nintendo)-money)+Nintendo, "to afford an additional Nintendo Wii"
+
+
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+int=input("Enter your number here: ")
+fact=1
+
+for i in range(1,int+1):
+    fact=fact*i
+print("The factorial of your number", int, "is", fact)
+
+
+
